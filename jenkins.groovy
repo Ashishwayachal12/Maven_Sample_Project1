b7f545c92dfd5266ea338c794c6dac00d658b14c
ASHISH  WAYACHAL <154004776+Ashishwayachal12@users.noreply.github.com>
2026-03-05 09:54:11 +0530

    Remove pwd command from Check Workspace stage
    Removed the 'sh' command to print working directory.

---
+++ jenkins.groovy
@@ -0,0 +1,38 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Check out') {
+            steps {
+                git 'https://github.com/Ashishwayachal12/Maven_Sample_Project1.git'
+                echo 'repo clone successful'
+            }
+        }
+        stage('fileExists'){
+            steps{
+                fileExists '/var/lib/jenkins/workspace/Maven_Project1'
+            }
+        }
+        stage('Maven Build'){
+            steps{
+                sh 'mvn clean install'
+            }
+        }
+        stage('Run Jar'){
+            steps{
+                 sh 'java -jar target/java-project2-2.1.jar'
+            }
+        }
+         stage('Clean Workspace'){
+            steps{
+               cleanWs()
+            }
+        }
+         stage('Check Workspace'){
+            steps{
+                fileExists '/var/lib/jenkins/workspace/Maven_Project1'
+                echo 'workspace deleted successfully'
+            }
+        }
+    }
+}
