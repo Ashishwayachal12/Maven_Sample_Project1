0e560b3cb2e19c04c375a62dfa955447e9fb47c7
ASHISH  WAYACHAL <154004776+Ashishwayachal12@users.noreply.github.com>
2026-04-02 15:52:41 +0530

    Change greeting message in App.java

--- src/main/java/com/louder/App.java
+++ src/main/java/com/louder/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello buddy! i am jenkins" );
+        System.out.println( "Hello buddy!" );
     }
 }
